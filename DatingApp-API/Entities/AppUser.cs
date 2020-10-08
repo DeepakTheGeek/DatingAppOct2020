5d6eccf23c4a897b7c61097af684de9d0785df9d
Deepak Das <Deepak.Das@brandmuscle.com>
2020-10-08 23:08:02 +0530

    first entity

---
+++ DatingApp-API/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp_API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        //Added uppercase N here in order to do less code refactoring when I will Identity model.
+        public string UserName { get; set; }
+    }
+}
