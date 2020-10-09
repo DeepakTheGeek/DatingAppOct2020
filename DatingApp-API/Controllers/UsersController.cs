aa0bb4721b656adea74b860ef9fdfba83c3b164e
Deepak Das <Deepak.Das@brandmuscle.com>
2020-10-10 01:43:01 +0530

    UsersController Created

---
+++ DatingApp-API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using DatingApp_API.Data;
+using DatingApp_API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUsers(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
